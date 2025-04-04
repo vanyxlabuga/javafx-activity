a904e623dc9f328c41d5418ac631f34e21714278
Manuel-PC <Manuel-PC@MANZMARK-PC>
2025-04-04 12:53:34 +0800

    Revert "first commit"
    This reverts commit 1b223ffa

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module aclcbukidnon.com.javafxactivity {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens aclcbukidnon.com.javafxactivity to javafx.fxml;
-    opens aclcbukidnon.com.javafxactivity.controllers to javafx.fxml;
-    exports aclcbukidnon.com.javafxactivity;
-}
